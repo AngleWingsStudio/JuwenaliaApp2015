6607b58f3dc361d79ff1d586d308c593a531c638
spekulant <tomaszbenq@gmail.com>
2015-02-16 21:59:52 +0100

    Creating a base project

---
+++ Juwenalia app/Juwenalia app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Juwenalia app
+//
+//  Created by Tomasz Sobota on 16.02.2015.
+//  Copyright (c) 2015 AngelWingsStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

d81f3df2d89eed1da0bbbf10f66a52a76067148b
monnalisa <monna.lisa94@gmail.com>
2015-03-08 20:15:52 +0100

    nowe

---
+++ Juwenalia app/PlanJuwenalii.h
@@ -0,0 +1,17 @@
+//
+//  PlanJuwenalii.h
+//  Juwenalia app
+//
+//  Created by Monika Kastner on 24.02.2015.
+//  Copyright (c) 2015 AngelWingsStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlanJuwenalii : UIViewController
+{
+
+    IBOutlet UIScrollView *scroller;}
+
+
+@end
---
+++ Juwenalia app/PlanJuwenalii.m
@@ -0,0 +1,26 @@
+//
+//  PlanJuwenalii.m
+//  Juwenalia app
+//
+//  Created by Monika Kastner on 24.02.2015.
+//  Copyright (c) 2015 AngelWingsStudio. All rights reserved.
+//
+
+#import "PlanJuwenalii.h"
+
+@interface PlanJuwenalii()
+
+@end
+
+@implementation PlanJuwenalii
+
+
+-(void)viewDidLoad {
+  [super viewDidLoad];
+    
+    [scroller setScrollEnabled:YES];
+    [scroller setContentSize:CGSizeMake(320, 580)];
+    
+}
+
+@end

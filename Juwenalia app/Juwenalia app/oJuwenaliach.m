5602df4c3777c29319cb109fcb06eea718aefd54
neegra <neegra@MacBook-Pro-Kinga.local>
2015-04-03 10:57:45 +0200

    proba

--- Juwenalia app/Juwenalia app/oJuwenaliach.m
+++ Juwenalia app/Juwenalia app/oJuwenaliach.m
@@ -7,13 +7,23 @@
 //
 
 #import "oJuwenaliach.h"
+#import "SWRevealViewController.h"
 
 @implementation oJuwenaliach
 
 -(void)viewDidLoad {
     [super viewDidLoad];
     
+    SWRevealViewController *revealViewController = self.revealViewController;
+    if ( revealViewController )
+    {
+        [self.sidebarButton setTarget: self.revealViewController];
+        [self.sidebarButton setAction: @selector( revealToggle: )];
+        [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+    }
+
+    
     [scroller setScrollEnabled:YES];
-    [scroller setContentSize:CGSizeMake(320, 624)];
+    [scroller setContentSize:CGSizeMake(320, 700)];
 }
 @end
